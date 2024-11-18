68f763126ed5666ac98fa0b18fc952386c371ce6
abhinav1524 <83525313+abhinav1524@users.noreply.github.com>
2024-11-18 10:13:31 +0530

    Update server.js
    adding methods and allowHeaders in corsoptions

--- backend/server.js
+++ backend/server.js
@@ -27,6 +27,8 @@ const app = express();
 const corsOptions = {
     origin: ['http://localhost:5173', 'https://pcecommerce.onrender.com'], // your frontend URL
     credentials: true, // Allow cookies to be sent
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
   };
 app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: true })); // handle simple form data without image
